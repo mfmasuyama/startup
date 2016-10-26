bae35426f1acb30de3b263b6ee0ff61b18b6662a
mfmasuyama <mfmasuyama@gmail.com>
2016-10-26 17:40:00 -0300

    Updates on Modulo 1.

---
+++ 01-JS-DOM-APIs/1/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".hidden").fadeIn(2000);
+});
+
+/*window.onload = funcion() {
+    var hiddenSection = document.body.getElementsByClassName("hidden")
+    hiddenSection.style.display="none";
+}*/
